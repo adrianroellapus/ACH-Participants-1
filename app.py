1edd37be2fc84d499656b467b759348d7f367e32
adrianroellapus <adrianlapus@gmail.com>
2026-02-03 14:07:38 +0800

    Update logo image width to 100 pixels
    Increased the width of the logo image from 90 to 100 pixels.

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 13])
 
 with col1:
-    st.image("bsp_logo.png", width=90)
+    st.image("bsp_logo.png", width=100)
 
 with col2:
     st.markdown("## ACH Participants Dashboard")
